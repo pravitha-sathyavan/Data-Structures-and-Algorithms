90065e3bc62e92e052e0bfb47dd85904ffcbf36e
Pravitha Sathyavan <46380586+pravitha-sathyavan@users.noreply.github.com>
2023-07-10 08:42:50 +0700

    RemoveDuplicates2

--- RemoveDuplicates2.java
+++ RemoveDuplicates2.java
@@ -3,13 +3,23 @@ class Solution {
         int k = 0;
         int i;
         int n = nums.length;
+        int count = 1;
         for(i = 1; i< n; ){
-            if(nums[k] == nums[i])
+            if(nums[k] == nums[i] && count == 2){
                i++;
-            else{
+               count = 2;
+            }
+            else if(nums[k] == nums[i]){
+                nums[k+1]=nums[i];
+                i++;
+                k++;
+                count++;
+            }
+            else {
                 nums[k+1] = nums[i];
                 k++;
                 i++;
+                count = 1;
             }
         }
         return k+1;
