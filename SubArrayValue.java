b86033b66ad53092096a30cedc30e5ce83ca0965
Pravitha Sathyavan <46380586+pravitha-sathyavan@users.noreply.github.com>
2023-10-11 16:36:45 +0530

    SubArrayValue.java
    https://practice.geeksforgeeks.org/contest/job-a-thon-hiring-challenge-experienced-professionals/problems
    
    Jobathon 23 geeks for geeks 2nd question

---
+++ SubArrayValue.java
@@ -0,0 +1,31 @@
+//Code not running
+
+
+class Solution {
+    public static int ValueOfSubarrays(int N, int[] arr) {
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
+        List<Integer> temp2 = new ArrayList<>();
+        for(int i=0;i<N;i++){
+            List<Integer> temp = new ArrayList<>();
+            temp2 = map.getOrDefault(arr[i],temp);
+            temp2.add(i);
+            map.put(arr[i],temp2);
+        }
+        long prev = -1;
+        long value = 0;
+        long mod = 1000000007;
+        for(int i: map.keySet()){
+            prev = -1;
+            List<Integer> temp = new ArrayList<>();
+             temp = map.get(i);
+            if(temp.size()==1)
+               continue;
+            for(long j=0;j<temp.size()-1;j++){
+                value += (temp.get(i)-prev)*(N-temp.get(i+1));
+                prev = temp.get(i);
+                value = value % mod;
+            }
+        }
+        return (int)value;
+    }
+}
