bcc04a9b4fa00096667166360b50f2414ec1514a
Pravitha Sathyavan <46380586+pravitha-sathyavan@users.noreply.github.com>
2023-10-22 20:00:39 +0530

    SingleElement2.java

--- SingleElement2.java
+++ SingleElement2.java
@@ -41,3 +41,66 @@ class Solution
      
     }
 }
+
+
+
+/*
+Given an unsorted array Arr of size N of positive integers. 
+One number 'A' from set {1, 2,....,N} is missing and one number 'B' occurs twice in array. 
+Find these two numbers.
+*/
+
+static void getTwoElements(int arr[], int n)
+    {
+        /* Will hold xor of all elements
+       and numbers from 1 to n  */
+        int xor1;
+
+        /* Will have only single set bit of xor1 */
+        int set_bit_no;
+
+        int i;
+        x = 0;
+        y = 0;
+
+        xor1 = arr[0];
+
+        /* Get the xor of all array elements  */
+        for (i = 1; i < n; i++)
+            xor1 = xor1 ^ arr[i];
+
+        /* XOR the previous result with numbers from 
+       1 to n*/
+        for (i = 1; i <= n; i++)
+            xor1 = xor1 ^ i;
+
+        /* Get the rightmost set bit in set_bit_no */
+        set_bit_no = xor1 & ~(xor1 - 1);
+
+        /* Now divide elements into two sets by comparing
+    rightmost set bit of xor1 with the bit at the same 
+    position in each element. Also, get XORs of two
+    sets. The two XORs are the output elements. The 
+    following two for loops serve the purpose */
+        for (i = 0; i < n; i++) {
+            if ((arr[i] & set_bit_no) != 0)
+                /* arr[i] belongs to first set */
+                x = x ^ arr[i];
+
+            else
+                /* arr[i] belongs to second set*/
+                y = y ^ arr[i];
+        }
+        for (i = 1; i <= n; i++) {
+            if ((i & set_bit_no) != 0)
+                /* i belongs to first set */
+                x = x ^ i;
+
+            else
+                /* i belongs to second set*/
+                y = y ^ i;
+        }
+
+        /* *x and *y hold the desired output elements */
+    }
+
