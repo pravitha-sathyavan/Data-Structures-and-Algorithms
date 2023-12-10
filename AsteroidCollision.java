c43cb92b1014d750d773ee954f8ad726fca6860a
Pravitha Sathyavan <46380586+pravitha-sathyavan@users.noreply.github.com>
2023-12-10 08:55:15 +0530

    AsteroidCollision

--- AsteroidCollision.java
+++ AsteroidCollision.java
@@ -1,3 +1,22 @@
+/*
+Example 1:
+
+Input: asteroids = [5,10,-5]
+Output: [5,10]
+Explanation: The 10 and -5 collide resulting in 10. The 5 and 10 never collide.
+Example 2:
+
+Input: asteroids = [8,-8]
+Output: []
+Explanation: The 8 and -8 collide exploding each other.
+Example 3:
+
+Input: asteroids = [10,2,-5]
+Output: [10]
+Explanation: The 2 and -5 collide resulting in -5. The 10 and -5 collide resulting in 10.
+
+*/
+
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
         int n = asteroids.length;
