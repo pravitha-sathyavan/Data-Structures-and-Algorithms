da8080d938794abb235c883eac5eec7d091beb67
Pravitha Sathyavan <46380586+pravitha-sathyavan@users.noreply.github.com>
2023-12-12 09:21:54 +0530

    Update MaximumSumofDistinctSubArraywithKelements.java

--- MaximumSumofDistinctSubArraywithKelements.java
+++ MaximumSumofDistinctSubArraywithKelements.java
@@ -1,38 +1,75 @@
+/*
+
+2461. Maximum Sum of Distinct Subarrays With Length K
+Medium
+1.1K
+16
+Companies
+You are given an integer array nums and an integer k. Find the maximum subarray sum of all the subarrays of nums that meet the following conditions:
+
+The length of the subarray is k, and
+All the elements of the subarray are distinct.
+Return the maximum subarray sum of all the subarrays that meet the conditions. If no subarray meets the conditions, return 0.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,5,4,2,9,9,9], k = 3
+Output: 15
+Explanation: The subarrays of nums with length 3 are:
+- [1,5,4] which meets the requirements and has a sum of 10.
+- [5,4,2] which meets the requirements and has a sum of 11.
+- [4,2,9] which meets the requirements and has a sum of 15.
+- [2,9,9] which does not meet the requirements because the element 9 is repeated.
+- [9,9,9] which does not meet the requirements because the element 9 is repeated.
+We return 15 because it is the maximum subarray sum of all the subarrays that meet the conditions
+Example 2:
+
+Input: nums = [4,4,4], k = 3
+Output: 0
+Explanation: The subarrays of nums with length 3 are:
+- [4,4,4] which does not meet the requirements because the element 4 is repeated.
+We return 0 because no subarrays meet the conditions.
+ 
+
+Constraints:
+
+1 <= k <= nums.length <= 105
+1 <= nums[i] <= 105
+    
+    */
 class Solution {
     public long maximumSubarraySum(int[] nums, int k) {
-        int leftP = 0;
-        long maxSum = 0;
-        long sum = 0;
-        int i = 0; 
+        long cSum = 0, maxSum = 0;
+        int leftP = 0, rightP = 0;
         HashMap<Integer,Integer> map = new HashMap<>();
+        int[] prefixSum = new int[nums.length];
+        prefixSum[0] = nums[0];
 
-        for(i=0;i<nums.length;i++){
-          if(k>0 && !map.containsKey(nums[i])){
-                map.put(nums[i],i);
-                sum = sum + nums[i];
-                k--;
-          }
-          else if(k==0 && !map.containsKey(nums[i])){
-                map.remove(nums[leftP]);
-                map.put(nums[i],i);
-                sum = sum - nums[leftP] + nums[i];
-                leftP++;
-          }
-          else if(map.containsKey(nums[i])){
-                int lastIndex = map.get(nums[i]);
-                for(int j = leftP; j<= lastIndex; j++){
-                    map.remove(nums[j]);
-                    sum = sum - nums[j];
-                    k++;
+        for(int i=0; i<nums.length; i++){
+            if(i!=0)
+                 prefixSum[i] = prefixSum[i-1]+nums[i];
+
+            if(map.containsKey(nums[i])){
+                leftP = Math.max(map.get(nums[i])+1,leftP);
+                cSum = prefixSum[i] - prefixSum[leftP-1];
+            }else{
+                cSum = cSum + nums[i];
+            }
+            rightP = i;
+            map.put(nums[i],i);
+
+            if(rightP - leftP == k - 1){
+                if(maxSum<cSum){
+                    maxSum = cSum;
                 }
-                leftP = lastIndex+1;
-                map.put(nums[i],i);
-                sum = sum + nums[i];
-                k--;
-          }
-          if(k==0 && maxSum < sum)
-                maxSum = sum;   
-           
+                cSum -= nums[leftP];
+                map.remove(nums[leftP]);
+                leftP++;
+            }
         }
 
         return maxSum;
