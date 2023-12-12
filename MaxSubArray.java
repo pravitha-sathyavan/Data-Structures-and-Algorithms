c2e8d533dea49bdfabc2c60b4f176b3745b6e840
Pravitha Sathyavan <46380586+pravitha-sathyavan@users.noreply.github.com>
2023-12-12 08:25:21 +0530

    Update MaxSubArray.java

--- MaxSubArray.java
+++ MaxSubArray.java
@@ -1,3 +1,18 @@
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int cSum = 0, maxSum = Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            cSum += nums[i];
+            if(maxSum<cSum)
+                maxSum = cSum;
+            if(cSum<0)
+                cSum = 0;
+        }
+        return maxSum;
+    }
+}
+
 class Solution {
     public int maxSubArray(int[] nums) {
         int max = nums[0];
@@ -9,3 +24,5 @@ class Solution {
         return max;        
     }
 }
+
+
