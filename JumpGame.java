7a6f95b0e045195a27aa7f23bcb388eda67c1059
Pravitha Sathyavan <46380586+pravitha-sathyavan@users.noreply.github.com>
2023-12-13 12:28:10 +0530

    Update JumpGame.java

--- JumpGame.java
+++ JumpGame.java
@@ -1,10 +1,37 @@
+/*
+You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+
+Return true if you can reach the last index, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: nums = [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+Example 2:
+
+Input: nums = [3,2,1,0,4]
+Output: false
+Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+0 <= nums[i] <= 105
+    
+    
+*/
 class Solution {
     public boolean canJump(int[] nums) {
-        int maxJumpfrombeg = 0;
-        for(int i=0;i<=maxJumpfrombeg;i++){
-            maxJumpfrombeg = Math.max(nums[i]+i,maxJumpfrombeg);
-            if(maxJumpfrombeg >= nums.length - 1)
-              return true;
+        int i=0, cm = 0;
+        while(i<=cm){
+            cm = Math.max(cm,(i+nums[i]));
+            if(cm>=nums.length-1)
+                return true;
+            i++;
         }
         return false;
     }
